67c03ecc68c40f9a0b363ccde824352245ff9228
Andy Teucher <andy.teucher@gov.bc.ca>
2016-09-14 16:28:46 -0700

    Start processing biogeoclimatic zones

--- 01_clean.R
+++ 01_clean.R
@@ -45,6 +45,8 @@ mock_ld_agg_simp <- geojson_json(mock) %>%
   fix_self_intersect() %>%
   transform_bc_albers()
 
+################################################################################
+
 ## Ecoregions:
 m_ecoregions <- c("HCS", "IPS", "OPS", "SBC", "TPC")
 
@@ -70,6 +72,44 @@ ecoregions_t_simp <- ms_simplify(ecoregions_t, 0.05) %>%
 ecoregions_t_simp_leaflet <- ms_simplify(ecoregions_t[,"CRGNCD"], 0.01) %>%
   fix_self_intersect() %>%
   spTransform(CRSobj = CRS("+init=epsg:4326"))
+
+###############################################################################
+## Biogeoclimatic zones
+if (!file.exists("data/BEC_BIOGEOCLIMATIC_POLY.gdb")) {
+  unzip("data/BCGW_78757263_1473884441500_9556.zip", exdir = "data")
+}
+
+bgc <- readOGR("data/BEC_BIOGEOCLIMATIC_POLY.gdb",
+               "WHSE_FOREST_VEGETATION_BEC_BIOGEOCLIMATIC_POLY_polygon",
+               stringsAsFactors = FALSE)
+writeOGR(bgc, "data", "bec", driver = "ESRI Shapefile")
+
+unlink(paste0("data/", c("bc_bound.geojson", "bec_clip*")))
+geojsonio::geojson_write(bc_bound_hres, file = "data/bc_bound.geojson") # bc_bound_hres is from bcmaps package
+system("mapshaper data/bec.shp -clip data/bc_bound.geojson -explode -o data/bec_clip.shp")
+
+## Check for validity of bec polygons
+bec_t <- readOGR("data", "bec_clip", stringsAsFactors = FALSE)
+bec_t <- fix_self_intersect(bec_t)
+
+## Simplify BEC pologyons for use in display
+bec_t$poly_id <- row.names(bec_t)
+unlink("data/bec_t*")
+writeOGR(bec_t, "data", "bec_t", "ESRI Shapefile")
+system("mapshaper data/bec_t.shp -simplify 0.01 keep-shapes -o data/bec_t_simp.shp")
+bec_t_simp <- readOGR("data", "bec_t_simp", stringsAsFactors = FALSE)
+## Repair orphaned holes
+bec_t_simp <- fix_self_intersect(bec_t_simp)
+
+## Put area back in m2
+bec_t$area <- gArea(bec_t, byid = TRUE)
+bec_t_simp$area <- bec_t$area
+
+## Create a map of bec zones
+bec_zone_simp <- raster::aggregate(bec_t_simp, by = "ZONE")
+bec_zone_simp$area <- gArea(bec_zone_simp, byid = TRUE)
+
+foo <- ms_simplify(bec_zone_simp, 0.05, keep_shapes = TRUE, explode = TRUE)
 
 saveRDS(mock, "tmp/mock_spatial.rds")
 saveRDS(mock_ld_agg, "tmp/mock_spatial_agg.rds")
