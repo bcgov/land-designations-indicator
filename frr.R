99c2c8e7682a4dc2c6298f5fa97697a47cb8acc4
Andy Teucher <andy.teucher@gov.bc.ca>
2016-10-28 17:29:39 -0700

    Begin processing new cl layer

--- frr.R
+++ frr.R
@@ -1,74 +1,112 @@
 library(rgdal)
+library(rgeos)
 library(raster)
 library(bcmaps)
 library(rmapshaper)
 library(parallel)
 library(ggplot2)
+library(ggpolypath)
 library(ggthemes)
+library(dplyr)
 
 source("fun.R")
 
+
 ## Get the spatial file in - read from saved rds if exists, otherwise process raw file
-frr_rds <- "tmp/frr.rds"
-if (!file.exists(frr_rds)) {
-  frr <- readOGR("data/rr_forAndy.gdb", "rr_restriction", stringsAsFactors = FALSE)
+cl_rds <- "tmp/cl.rds"
+cl_clip_rds <- "tmp/cl_clip.rds"
+if (!file.exists(cl_rds)) {
+  cl <- readOGR("data/conservation_lands", "conservation_lands", stringsAsFactors = FALSE)
 
-  frr2 <- frr[!is.na(frr$rr_restriction_rollup), ] # Remove non-designated areas
-  frr2 <- fix_self_intersect(frr2)
+  cl <- fix_geo_problems(cl)
 
-  frr2 <- transform_bc_albers(frr2)
-  saveRDS(frr2, frr_rds)
-  rm(frr)
+  saveRDS(cl, cl_rds)
 } else {
-  frr2 <- readRDS(frr_rds)
+  cl <- readRDS(cl_rds)
 }
 
-## Aggregate by rollup group
-frr_agg_rds <- "tmp/frr_agg.rds"
-if (!file.exists(frr_agg_rds)) {
-  frr_agg <- aggregate(frr2[, "rr_restriction_rollup"], by = list(cons_cat = frr2$rr_restriction_rollup), FUN = max)
-  saveRDS(frr_agg, "tmp/frr_agg.rds")
+cat_rollup_table <- unique(cl@data[, c("category", "rollup")])
+
+if (!file.exists(cl_clip_rds)) {
+  cl_u <- gUnaryUnion(cl, id = cl$category)
+  cl_u_clip <- gIntersection(cl_u, bc_bound_hres, byid = TRUE)
+  row.names(cl_u_clip) <- sapply(strsplit(row.names(cl_u_clip), " "), `[`, 1)
+  cl_u_clip <- SpatialPolygonsDataFrame(cl_u_clip, cat_rollup_table, match.ID = "category")
+  cl_u_clip$area <- gArea(cl_u_clip, byid = TRUE)
+  saveRDS(cl_u_clip, cl_clip_rds)
 } else {
-  frr_agg <- readRDS(frr_agg_rds)
+  cl_u_clip <- readRDS(cl_clip_rds)
 }
 
-ld_agg <- frr_agg
-rm(frr2, frr_agg)
+eco_x_cl_rds <- "tmp/ecoreg_x_cl.rds"
+if (!file.exists(eco_x_cl_rds)) {
+  ecoregions_t <- readRDS("tmp/ecoregions_t.rds")
+  system.time(eco_cl_int <- parallel_apply(cl_u_clip, "category",
+                                           raster::intersect,
+                                           y = ecoregions_t,
+                                           recombine = TRUE)) # This took 13 hours
+  eco_cl_int$area <- gArea(eco_cl_int, byid = TRUE)
+  saveRDS(eco_cl_int, eco_x_cl_rds)
+} else {
+  eco_cl_int <- readRDS(eco_x_cl_rds)
+}
+
+eco_cl_summary <- eco_cl_int@data %>%
+  group_by(CRGNNM, rollup) %>%
+  summarize(area_ha = sum(area, na.rm = TRUE) * 1e-4,
+            percent = sum(area, na.rm = TRUE) / mean(area.2, na.rm = TRUE) * 100)
 
 ## Simplify the aggregated designation rollup polygons
-frr_agg_list_simp_rds <- "tmp/frr_agg_list_simp.rds"
-if (!file.exists(frr_agg_list_simp_rds)) {
+cl_simp_rds <- "tmp/cl_simp.rds"
+if (!file.exists(cl_simp_rds)) {
 
-  # Split into a list so objects passed into ms_simplify are smaller. Also can be parallelized this way
-  ld_agg_list <- lapply(ld_agg$cons_cat[ld_agg$cons_cat != "06_No Special Restriction"], function(x) {
-    ld_agg[ld_agg$cons_cat == x,]
-  })
+  cl_simp <- parallel_apply(cl_u_clip, "category", ms_simplify,
+                                keep = 0.01, keep_shapes = TRUE,
+                                recombine = TRUE)
 
-  ## Do the simplification in Parallel:
-  no_cores <- detectCores() - 1 # Use one less than max cores so we have computing capacity to do other things
-  cl <- makeCluster(no_cores)
+  cl_simp <- fix_geo_problems(cl_simp)
 
-  ld_agg_list_simp <- parLapply(cl, ld_agg_list, ms_simplify, keep = 0.01, keep_shapes = TRUE)
-  stopCluster(cl)
-
-  ld_agg_list_simp <- lapply(ld_agg_list_simp, fix_self_intersect)
-
-  saveRDS(ld_agg_list_simp, frr_agg_list_simp_rds)
+  saveRDS(cl_simp, cl_simp_rds)
 } else {
-  ld_agg_list_simp <- readRDS(frr_agg_list_simp_rds)
+  cl_simp <- readRDS(cl_simp_rds)
 }
 
-## Recombine list elements into one simplified sp object
-ld_agg_simp <- ld_agg_list_simp[[1]]
-for (i in 2:length(ld_agg_list_simp)) {
-  ld_agg_simp <- rbind(ld_agg_simp, ld_agg_list_simp[[i]])
-}
+simp_zone_intersect <- parallel_apply(cl_simp, "category",
+                                      raster::intersect,
+                                      y = bec_zone_simp,
+                                      recombine = TRUE)
+
+simp_zone_intersect$area <- gArea(simp_zone_intersect, byid = TRUE)
+
+test_simp_zone_summmary <- simp_zone_intersect@data %>%
+  group_by(ZONE, rollup) %>%
+  summarize(area_ha = sum(area, na.rm = TRUE) * 1e-4,
+            percent = sum(area, na.rm = TRUE) / mean(area.1, na.rm = TRUE) * 100)
+
+summary <- cl_u_clip@data %>%
+  group_by(rollup) %>%
+  summarise(area_designated = sum(area, na.rm = TRUE),
+            percent_of_bc = round(area_designated / gArea(bc_bound_hres) * 100, 2))
+readr::write_csv(summary, "out/total_provincial_summary.csv")
+
 
 ## Convert to ggplot2-friendly data frame, and plot
-gg_ld_agg <- gg_fortify(ld_agg_simp)
 
-ggplot(gg_ld_agg, aes(x = long, y = lat, group = group, fill = cons_cat)) +
-  geom_polygon() +
+gg_bc <- gg_fortify(bc_bound_hres)
+
+gg_cl <- gg_fortify(cl_simp)
+
+cols <- c("#004529", "#238443", "#addd8e", "#f7fcb9")
+labels = c("01_PPA"                    = "Parks and Protected Areas",
+           "02_Protected_Other"        = "Other Protected Lands",
+           "03_Exclude_1_2_Activities" = "Exclude 1 or 2 Actitivies",
+           "04_Managed"                = "Managed Lands")
+
+ggplot(gg_bc, aes(x = long, y = lat, group = group)) +
+  geom_polygon(colour = "grey70", fill = "grey80") +
+  geom_polypath(data = gg_cl, aes(x = long, y = lat, group = group, fill = rollup)) +
+  scale_fill_manual(name = "Land Designation Category",
+                    values = cols, labels = labels) +
   coord_fixed() +
   theme_map()
 
--- fun.R
+++ fun.R
@@ -1,11 +1,11 @@
 # Copyright 2016 Province of British Columbia
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
@@ -38,4 +38,58 @@ gg_ld_ecoreg <- function(ecoreg_cd, ld_df, ecoreg_df) {
     ggtitle(tools::toTitleCase(tolower(ecoreg_df_sub$CRGNNM[1]))) +
     coord_fixed() +
     theme_map()
+}
+
+
+
+#' Split a large SpatialPolygonsDataFrame into a list and apply a mapshaper function
+#' to each element in Parallel
+#'
+#' @param spdf
+#' @param column
+#' @param fun
+#' @param ...
+#' @param recombine
+#'
+#' @return list of SPDFs if recombine is FALSE, a SPDF if TRUE
+#' @export
+#'
+#' @examples
+#' library(bcmaps)
+#' foo <- parallel_mapshaper(ecoprovinces, "CPRVNCCD", ms_simplify, keep_shapes = TRUE, recombine = FALSE)
+#' lapply(foo, plot)
+#' bar <- recombine_spatial_list(foo)
+parallel_apply <- function(spdf, column, fun = ms_simplify, ..., recombine = FALSE) {
+
+  spdf_list <- split_on_attribute(spdf, column)
+
+  ## Do the simplification in Parallel:
+  no_cores <- detectCores() - 1 # Use one less than max cores so we have computing capacity to do other things
+  cl <- makeCluster(no_cores)
+
+  on.exit(stopCluster(cl))
+
+  spdf_list_out <- parLapply(cl, spdf_list, fun, ...)
+
+  if (!recombine) return(spdf_list_out)
+
+  combine_spatial_list(spdf_list_out)
+}
+
+split_on_attribute <- function(spdf, column) {
+  lapply(spdf[[column]], function(x) {
+    spdf[spdf[[column]] == x,]
+  })
+}
+
+#' Combine a list of Spatial objects into one
+#'
+#' @param splist a list of Spatial objects, all of the same type
+#' @param ... arguments passed on to \code{link[raster]{bind}}
+#'
+#' @return a Spatial object of the same class as the inputs
+#' @export
+combine_spatial_list <- function(splist, ...) {
+  ## Recombine list elements into one sp object
+  do.call(raster::bind, splist, ...)
 }
