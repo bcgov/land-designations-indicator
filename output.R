aa226fc3ba3ed5489845b5783048cd60230a1a4f
Andy Teucher <andy.teucher@gov.bc.ca>
2016-12-09 08:30:49 -0800

    Specify app files

--- output.R
+++ output.R
@@ -1,1 +1,2 @@
-file.copy(from = list.files("out", full.names = TRUE), to = "../land-designations-shinyapp/data/", overwrite = TRUE)
+files_list <- list.files("out", "\\.(rds$|feather$)", full.names = TRUE)
+file.copy(from = files_list, to = "../land-designations-shinyapp/app/data", overwrite = TRUE)
