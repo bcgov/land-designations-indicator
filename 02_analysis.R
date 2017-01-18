9c4b5ad8a5a2125b72399adf7ce8884962989a63
Andy Teucher <andy.teucher@gov.bc.ca>
2017-01-17 17:32:59 -0800

    Clean up data summary code, write to csv as well as feather

--- 02_analysis.R
+++ 02_analysis.R
@@ -35,7 +35,8 @@ bc_ld_summary <- ld_t@data %>%
   summarize(area_des_ha = sum(area) * 1e-4) %>%
   mutate(percent_des = (area_des_ha * 1e4) / sum(bc_bound_trim$SHAPE_Area) * 100) %>%
   mutate_if(is.numeric, round, digits = 2) %>%
-write_feather("out-shiny/bc_ld_summary.feather")
+  write_feather("out-shiny/bc_ld_summary.feather") %>%
+  write_csv("out/bc_land_designations_summary.csv")
 
 ################################################################################
 # BEC
@@ -54,20 +55,36 @@ bec_cat_summary <- bec_ld_t@data %>%
            fill = list(area_des = 0, area_des_ha = 0, percent_des = 0)) %>%
   mutate(category = as.character(category)) %>%
   rename(MAP_LABEL = map_label) %>%
-  write_feather("out-shiny/ld_bec_summary.feather")
+  write_feather("out-shiny/ld_bec_summary.feather") %>%
+  write_csv("out/bc_bgc_land_designations_summary.csv")
 
 ################################################################################
-# Ecoregions
+# Ecoregions and ecosections
 
-eco_cat_summary <- eco_ld_t@data %>%
-  group_by(CRGNCD = parent_ecoregion_code, category = factor(category)) %>%
-  summarize(area_des = sum(shape_area, na.rm = TRUE)) %>%
-  complete(CRGNCD, category,
-           fill = list(area_des = 0)) %>%
-  mutate(category = as.character(category)) %>%
-  right_join(select(ecoregions_t@data, CRGNCD, ecoreg_area),
-             by = "CRGNCD") %>%
+eco_ld_full <- readOGR("data/lands_eco.gdb", stringsAsFactors = FALSE)
+ecosec_sizes <- eco_ld_full@data %>%
+  filter(bc_boundary == "bc_boundary_land_tiled") %>%
+  group_by(parent_ecoregion_code, ecosection_name, ecosection_code) %>%
+  summarize(ecosec_area = sum(shape_area, na.rm = TRUE))
+
+ecoreg_sizes <- ecosec_sizes %>%
+  group_by(parent_ecoregion_code) %>%
+  summarize(ecoreg_area = sum(ecosec_area, na.rm = TRUE))
+
+ecosection_cat_summary <- eco_ld_t@data %>%
+  right_join(ecosec_sizes, by = c("parent_ecoregion_code", "ecosection_name", "ecosection_code")) %>%
+  complete(nesting(parent_ecoregion_code, ecosection_name, ecosection_code, ecosec_area), category) %>%
+  group_by(category, parent_ecoregion_code, ecosection_name, ecosection_code) %>%
+  summarize(area_des = sum(shape_area, na.rm = TRUE),
+            ecosec_area = sum(ecosec_area, na.rm = TRUE),
+            percent_des = area_des / ecosec_area * 100,
+            area_des_ha = area_des * 1e-4) %>%
+  write_csv("out/bc_ecosections_land_designations_summary.csv")
+
+ecoregion_cat_summary2 <- ecosection_cat_summary %>%
+  group_by(parent_ecoregion_code, category) %>%
+  summarize(area_des = sum(area_des, na.rm = TRUE)) %>%
+  right_join(ecoreg_sizes, by = "parent_ecoregion_code") %>%
   mutate(percent_des = area_des / ecoreg_area * 100,
-         area_des_ha = area_des * 1e-4) %>%
-  write_feather("out-shiny/ld_ecoreg_summary.feather")
-
+            area_des_ha = area_des * 1e-4) %>%
+  write_csv("out/bc_ecoregions_land_designations_summary.csv")
