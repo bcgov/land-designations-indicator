94c8518b7315e997f989946cc6e18c8a15c47bcd
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-01-19 16:19:02 -0800

    bgc full names & print plot to png

--- output.R
+++ output.R
@@ -13,6 +13,7 @@
 library(ggplot2)
 library(ggpolypath)
 library(envreportutils) #theme_soe_facet
+library(magrittr) # %>%
 
 files_list <- list.files("out-shiny", full.names = TRUE)
 file.copy(from = files_list, to = "../land-designations-shinyapp/app/data", overwrite = TRUE)
@@ -35,7 +36,23 @@ des_cols <- c("01_PPA"                    = "#00441b",
               "03_Exclude_1_2_Activities" = "#a6d96a",
               "04_Managed"                = "#fdbf6f")
 
-
+bgc$bec_nms <- bgc$ZONE
+bgc$bec_nms[bgc$bec_nms == "BAFA"] <- "Boreal Altai Fescue Alpine"
+bgc$bec_nms[bgc$bec_nms == "SWB"] <- "Spruce—Willow—Birch"
+bgc$bec_nms[bgc$bec_nms == "BWBS"] <- "Boreal White & Black Spruce"
+bgc$bec_nms[bgc$bec_nms ==  "ESSF"] <- "Engelmann Spruce—Subalpine Fir"
+bgc$bec_nms[bgc$bec_nms == "CMA"] <- "Coastal Mountain-heather Alpine"
+bgc$bec_nms[bgc$bec_nms ==  "SBS"] <-  "Sub-Boreal Spruce"
+bgc$bec_nms[bgc$bec_nms ==  "MH"] <-  "Mountain Hemlock"
+bgc$bec_nms[bgc$bec_nms ==  "CWH"] <- "Coastal Western Hemlock"
+bgc$bec_nms[bgc$bec_nms ==  "ICH"] <- "Interior Cedar—Hemlock"
+bgc$bec_nms[bgc$bec_nms ==  "IMA"] <- "Interior Mountain-heather Alpine"
+bgc$bec_nms[bgc$bec_nms ==  "SBPS"] <- "Sub-Boreal Pine—Spruce"
+bgc$bec_nms[bgc$bec_nms ==  "MS"] <- "Montane Spruce"
+bgc$bec_nms[bgc$bec_nms ==  "IDF"] <- "Interior Douglas-fir"
+bgc$bec_nms[bgc$bec_nms ==  "BG"] <- "Bunchgrass"
+bgc$bec_nms[bgc$bec_nms ==  "PP"] <- "Ponderosa Pine"
+bgc$bec_nms[bgc$bec_nms ==  "CDF"] <- "Coastal Douglas-fir"
 
 ##function to roll-up two protected categories
 rollup_category <- function(category) {
@@ -52,19 +69,28 @@ lab <- c("04_Managed" = "Managed Areas",
          "03_Exclude_1_2_Activities" = "Exclude 1 or 2 Activities",
          "Prot" = "Protected Lands")
 
+bgc <- bgc  %>%
+    order_df("bec_nms", "percent_designated", fun = max)
+
 ##facet plot
-facetplot <- ggplot(bgc, aes(x = ZONE, y = percent_designated, fill = category)) +
+facetplot <- ggplot(bgc, aes(x = bec_nms, y = percent_designated, fill = category)) +
   geom_col() +
   facet_wrap(~rollup, nrow=3, labeller = labeller(rollup = lab)) +
   scale_fill_manual(values = des_cols, guide =FALSE) +
    coord_flip() +
   labs(x = "Biogeoclimatic Zone\n", y = "Percent Designated") +
-  scale_y_continuous(expand = c(0, 0), breaks = seq(0, 70, 10), limits = c(0, 70)) +
+  scale_y_continuous(expand = c(0, 0), breaks = seq(0, 65, 5), limits = c(0, 65)) +
   theme_soe_facet() +
-  theme(panel.grid.major.y = element_line(colour = "grey90"),
-        panel.grid.minor.y = element_line(colour = "grey90"),
-        panel.grid.major.x = element_blank(),
-        strip.placement = "top")
-#        plot.margin = unit(c(2,0,1,0), "lines"))
+  theme(panel.grid.major.x = element_line(colour = "grey90"),
+        panel.grid.minor.x = element_line(colour = "grey90"),
+        panel.grid.major.y = element_blank(),
+        strip.placement = "top",
+        plot.margin = unit(c(2,2,1,1), "lines"))
 plot(facetplot)
+
+## print facet plot to PNG
+png(filename = "out/bgc_facet_plot.png",
+    width = 900, height = 600, units = "px")
+facetplot
+dev.off()
 
