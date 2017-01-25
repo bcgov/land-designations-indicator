7d6919bdf4414ac8d5044b68fa8e5b7af31d5a06
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2017-01-25 12:17:58 -0800

    added static bc summary bar chart

--- output.R
+++ output.R
@@ -26,16 +26,77 @@ ggplot(gg_ld_ecoreg, aes(x = long, y = lat, group = group)) +
   geom_polypath(aes(fill = category)) +
   coord_fixed()
 
-## Static facet bar chart for summary by category
+## Static Outputs
 
-##read in datafile
-bgc <- read.csv("out/bc_bgc_zone_land_designations_summary.csv", stringsAsFactors = FALSE)
-
+## colour palette
 des_cols <- c("01_PPA"                    = "#00441b",
               "02_Protected_Other"        = "#006d2c",
               "03_Exclude_1_2_Activities" = "#a6d96a",
               "04_Managed"                = "#fdbf6f")
 
+## function to roll-up the two protected (sub)categories
+rollup_category <- function(category) {
+  factor(ifelse(category %in% c("01_PPA", "02_Protected_Other"),
+                "Prot", category),
+         levels = c("04_Managed", "03_Exclude_1_2_Activities", "Prot"), ordered = TRUE)
+}
+
+
+## Static bar chart for provincial summary by category
+## read in datafile
+bcsum <- read.csv("out/bc_land_designations_summary.csv", stringsAsFactors = FALSE)
+
+## roll-up two protected categories
+bcsum$rollup <- rollup_category(bcsum$category)
+
+## category labels for legend
+cat_labels <- c("01_PPA" = "Parks & Protected Areas",
+                "02_Protected_Other" = "Other Protected Lands",
+                "03_Exclude_1_2_Activities" = "Resource Exclusion Areas",
+                "04_Managed" = "Managed Areas")
+
+## setting factors for ordering bars
+bcsum$category <- factor(bcsum$category, levels = c("04_Managed",
+                                                    "03_Exclude_1_2_Activities",
+                                                    "02_Protected_Other",
+                                                    "01_PPA"))
+## bc summary bar plot
+bcsumplot <- ggplot(bcsum, aes(x = rollup, y = percent_des, fill = category)) +
+  geom_col() +
+  scale_fill_manual(name = "", values = des_cols, labels = cat_labels) +
+  scale_x_discrete(labels = c("04_Managed" = "Managed\nAreas",
+                              "03_Exclude_1_2_Activities" = "Resource Exclusion\nAreas",
+                              "Prot" = "Protected\nLands")) +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  coord_flip() +
+  labs(x = "", y = "Percent of British Columbia Designated") +
+  scale_y_continuous(expand = c(0, 0), breaks = seq(0, 25, 5), limits = c(0, 25)) +
+  theme_soe() +
+  theme(panel.grid.major.x = element_line(colour = "grey90"),
+        panel.grid.minor.x = element_line(colour = "grey90"),
+        panel.grid.major.y = element_blank(),
+        legend.position = "bottom",
+        legend.direction = "vertical",
+   #    legend.position = c(.85, .72),
+        legend.background = element_blank(),
+        legend.text = element_text(size = 12),
+        axis.title = element_text(size = 14),
+       axis.text = element_text(size = 12),
+       plot.margin = unit(c(2,3,1,1), "lines"))
+plot(bcsumplot)
+
+## print BC Summary plot to PNG
+png(filename = "out/bc_sum_plot.png",
+    width = 500, height = 500, units = "px")
+bcsumplot
+dev.off()
+
+
+## Static facet bar chart for summary by bgc and category
+##read in datafile
+bgc <- read.csv("out/bc_bgc_zone_land_designations_summary.csv", stringsAsFactors = FALSE)
+
+## Adding full BGC names to dataframe
 bgc$bec_nms <- bgc$ZONE
 bgc$bec_nms[bgc$bec_nms == "BAFA"] <- "Boreal Altai Fescue Alpine"
 bgc$bec_nms[bgc$bec_nms == "SWB"] <- "Spruce—Willow—Birch"
@@ -54,12 +115,67 @@ bgc$bec_nms[bgc$bec_nms ==  "BG"] <- "Bunchgrass"
 bgc$bec_nms[bgc$bec_nms ==  "PP"] <- "Ponderosa Pine"
 bgc$bec_nms[bgc$bec_nms ==  "CDF"] <- "Coastal Douglas-fir"
 
-##function to roll-up two protected categories
-rollup_category <- function(category) {
-  factor(ifelse(category %in% c("01_PPA", "02_Protected_Other"),
-                "Prot", category),
-         levels = c("04_Managed", "03_Exclude_1_2_Activities", "Prot"), ordered = TRUE)
-}
+
+##roll-up two protected categories
+bgc$rollup <- rollup_category(bgc$category)
+
+#facet labels
+lab <- c("04_Managed" = "Managed Areas",
+         "03_Exclude_1_2_Activities" = "Resource Exclusion Areas",
+         "Prot" = "Protected Lands")
+
+## order bars within facets
+bgc <- bgc  %>%
+    order_df("bec_nms", "percent_designated", fun = max)
+
+## bgc facet plot
+bgcfacetplot <- ggplot(bgc, aes(x = bec_nms, y = percent_designated, fill = category)) +
+  geom_col() +
+  facet_wrap(~rollup, nrow=3, labeller = labeller(rollup = lab)) +
+  scale_fill_manual(values = des_cols, guide =FALSE) +
+   coord_flip() +
+  labs(x = "Biogeoclimatic Zone\n", y = "Percent Designated") +
+  scale_y_continuous(expand = c(0, 0), breaks = seq(0, 65, 5), limits = c(0, 65)) +
+  theme_soe_facet() +
+  theme(panel.grid.major.x = element_line(colour = "grey90"),
+        panel.grid.minor.x = element_line(colour = "grey90"),
+        panel.grid.major.y = element_blank(),
+        strip.placement = "top",
+        strip.text.x = element_text(size = 13),
+        axis.title = element_text(size = 14),
+        axis.text = element_text(size = 11),
+        plot.margin = unit(c(2,2,1,1), "lines"))
+plot(bgcfacetplot)
+
+## print BGC facet plot to PNG
+png(filename = "out/bgc_facet_plot.png",
+    width = 900, height = 700, units = "px")
+bgcfacetplot
+dev.off()
+
+## Static facet bar chart for summary by ecoregion and category
+##read in datafile
+ecoregions <- read.csv("out/bc_ecoregions_land_designations_summary.csv", stringsAsFactors = FALSE)
+
+## Adding full BGC names to dataframe
+bgc$bec_nms <- bgc$ZONE
+bgc$bec_nms[bgc$bec_nms == "BAFA"] <- "Boreal Altai Fescue Alpine"
+bgc$bec_nms[bgc$bec_nms == "SWB"] <- "Spruce—Willow—Birch"
+bgc$bec_nms[bgc$bec_nms == "BWBS"] <- "Boreal White & Black Spruce"
+bgc$bec_nms[bgc$bec_nms ==  "ESSF"] <- "Engelmann Spruce—Subalpine Fir"
+bgc$bec_nms[bgc$bec_nms == "CMA"] <- "Coastal Mountain-heather Alpine"
+bgc$bec_nms[bgc$bec_nms ==  "SBS"] <-  "Sub-Boreal Spruce"
+bgc$bec_nms[bgc$bec_nms ==  "MH"] <-  "Mountain Hemlock"
+bgc$bec_nms[bgc$bec_nms ==  "CWH"] <- "Coastal Western Hemlock"
+bgc$bec_nms[bgc$bec_nms ==  "ICH"] <- "Interior Cedar—Hemlock"
+bgc$bec_nms[bgc$bec_nms ==  "IMA"] <- "Interior Mountain-heather Alpine"
+bgc$bec_nms[bgc$bec_nms ==  "SBPS"] <- "Sub-Boreal Pine—Spruce"
+bgc$bec_nms[bgc$bec_nms ==  "MS"] <- "Montane Spruce"
+bgc$bec_nms[bgc$bec_nms ==  "IDF"] <- "Interior Douglas-fir"
+bgc$bec_nms[bgc$bec_nms ==  "BG"] <- "Bunchgrass"
+bgc$bec_nms[bgc$bec_nms ==  "PP"] <- "Ponderosa Pine"
+bgc$bec_nms[bgc$bec_nms ==  "CDF"] <- "Coastal Douglas-fir"
+
 
 ##roll-up two protected categories
 bgc$rollup <- rollup_category(bgc$category)
@@ -70,14 +186,14 @@ lab <- c("04_Managed" = "Managed Areas",
          "Prot" = "Protected Lands")
 
 bgc <- bgc  %>%
-    order_df("bec_nms", "percent_designated", fun = max)
+  order_df("bec_nms", "percent_designated", fun = max)
 
 ##facet plot
 facetplot <- ggplot(bgc, aes(x = bec_nms, y = percent_designated, fill = category)) +
   geom_col() +
   facet_wrap(~rollup, nrow=3, labeller = labeller(rollup = lab)) +
   scale_fill_manual(values = des_cols, guide =FALSE) +
-   coord_flip() +
+  coord_flip() +
   labs(x = "Biogeoclimatic Zone\n", y = "Percent Designated") +
   scale_y_continuous(expand = c(0, 0), breaks = seq(0, 65, 5), limits = c(0, 65)) +
   theme_soe_facet() +
