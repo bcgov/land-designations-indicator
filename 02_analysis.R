75922f233d26a5eafaa910f6ed66d01cb2a3e055
Karly Harker <karly.harker@gov.bc.ca>
2021-02-26 07:30:08 -0800

    update area calc, data summaries

--- 02_analysis.R
+++ 02_analysis.R
@@ -1,4 +1,4 @@
-# Copyright 2016 Province of British Columbia
+# Copyright 2021 Province of British Columbia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,95 +10,81 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+## Land Designations Indicator - 01_analysis.R
+
 library(dplyr)
-library(tidyr)
-library(feather)
-library(readr)
+library(tidyverse)
 library(sf)
 
-source("fun.R")
+
+if (!exists("ld_m")) load("tmp/raw_data_vect.RData")
 
 dir.create("out", showWarnings = FALSE)
 dir.create("out-shiny", showWarnings = FALSE)
 
-## BC Summary
-bc_ld_summary <- ld_t %>%
+
+#Find area/proportion for each restriction level/type
+
+ld_m_sum <- ld_m %>%
+  mutate(total_area = st_area(geom)) %>%
   st_set_geometry(NULL) %>%
-  group_by(category) %>%
-  filter_non_designated() %>%
-  summarize(area_des_ha = as.numeric(sum(calc_area)) * 1e-4) %>%
-  mutate(percent_des = (area_des_ha * 1e4) / as.numeric(sum(ld_t$calc_area)) * 100) %>%
-  mutate_if(is.numeric, round, digits = 2) %>%
-  write_feather("out-shiny/bc_ld_summary.feather") %>%
-  write_csv("out/bc_land_designations_summary.csv")
-
-################################################################################
-# BEC
-
-bec_cat_summary <- bec_ld %>% st_set_geometry(NULL) %>%
-  filter_non_designated() %>%
-  group_by(map_label, category = factor(category)) %>%
-  summarize(area_des = sum(calc_area, na.rm = TRUE)) %>%
-  right_join(group_by(st_set_geometry(bec_t, NULL), MAP_LABEL, ZONE, ZONE_NAME,
-                      SUBZONE, SUBZONE_NAME, VARIANT, VARIANT_NAME) %>%
-               summarize(bec_area = sum(bec_area, na.rm = TRUE)),
-             by = c("map_label" = "MAP_LABEL")) %>%
-  mutate(percent_des = as.numeric(area_des / bec_area * 100),
-         area_des_ha = area_des * 1e-4) %>%
-  complete(nesting(map_label, ZONE, ZONE_NAME, SUBZONE, SUBZONE_NAME,
-                   VARIANT, VARIANT_NAME, bec_area), category,
-           fill = list(area_des = 0, area_des_ha = 0, percent_des = 0)) %>%
-  mutate(category = as.character(category)) %>%
-  rename(MAP_LABEL = map_label) %>%
-  write_feather("out-shiny/ld_bec_summary.feather") %>%
-  write_csv("out/bc_bgc_land_designations_summary.csv")
-
-bec_zone_cat_summary <- bec_cat_summary %>%
-  group_by(ZONE, ZONE_NAME, category) %>%
-  summarize(area_designated_ha = sum(area_des_ha, na.rm = TRUE),
-            zone_area_ha = sum(bec_area, na.rm = TRUE) * 1e-4,
-            percent_designated = as.numeric(area_designated_ha / zone_area_ha * 100)) %>%
-  mutate_if(is.numeric, round, digits = 2) %>%
-  arrange(ZONE, category) %>%
-  write_csv("out/bc_bgc_zone_land_designations_summary.csv")
+  group_by(mine_restriction) %>%
+  summarize(N=length(mine_restriction_id),
+          total_area_ha = round(sum(as.numeric(total_area)) / 10000, digits=2),
+          .groups = "drop")%>%
+  mutate(perc_bc_ha = round(total_area_ha/bc_size_ha*100, digits=1)) %>%
+  add_column(restriction_type = "Mining", .before="mine_restriction")%>%
+  rename(restriction = mine_restriction)
 
 
-bec_zone_sizes <- bec_t%>% st_set_geometry(NULL) %>%
-  group_by(ZONE) %>%
-  summarise(area = sum(bec_area, na.rm = TRUE) * 1e-4)
-################################################################################
-# Ecoregions and ecosections ## TODO - add ecosection_name back in
+ld_f_sum <- ld_f %>%
+  mutate(total_area = st_area(geom)) %>%
+  st_set_geometry(NULL) %>%
+  group_by(forest_restriction) %>%
+  summarize(N=length(forest_restriction_id),
+            total_area_ha = round(sum(as.numeric(total_area)) / 10000, digits=2),
+            .groups = "drop")%>%
+  mutate(perc_bc_ha = round(total_area_ha/bc_size_ha*100, digits=1))%>%
+  add_column(restriction_type = "Forestry", .before="forest_restriction")%>%
+  rename(restriction = forest_restriction)
 
-ecosec_sizes <- eco_ld %>% st_set_geometry(NULL) %>%
-  group_by(parent_ecoregion_code, ecosection_name,
-           ecosection_code) %>%
-  summarize(ecosec_area = sum(calc_area, na.rm = TRUE))
+ld_o_sum <- ld_o %>%
+  mutate(total_area = st_area(geom)) %>%
+  st_set_geometry(NULL) %>%
+  group_by(og_restriction) %>%
+  summarize(N=length(og_restriction_id),
+            total_area_ha = round(sum(as.numeric(total_area)) / 10000, digits=2),
+            .groups = "drop")%>%
+  mutate(perc_bc_ha = round(total_area_ha/bc_size_ha*100, digits=1))%>%
+  add_column(restriction_type = "Oil & Gas", .before="og_restriction") %>%
+  rename(restriction = og_restriction)
 
-ecoreg_sizes <- ecosec_sizes %>%
-  group_by(ecoregion_code = parent_ecoregion_code) %>%
-  summarize(ecoreg_area = sum(ecosec_area, na.rm = TRUE))
 
-ecosection_cat_summary <- eco_ld %>% st_set_geometry(NULL) %>%
-  filter_non_designated() %>%
-  right_join(ecosec_sizes, by = c("parent_ecoregion_code", "ecosection_name",
-                                  "ecosection_code")) %>%
-  complete(nesting(parent_ecoregion_code, ecosection_name,
-                   ecosection_code, ecosec_area), category) %>%
-  group_by(category, parent_ecoregion_code, ecosection_code, ecosection_name) %>%
-  summarize(area_des = sum(calc_area, na.rm = TRUE),
-            ecosec_area = sum(ecosec_area, na.rm = TRUE),
-            percent_des = as.numeric(area_des / ecosec_area * 100),
-            area_des_ha = area_des * 1e-4) %>%
-  write_csv("out/bc_ecosections_land_designations_summary.csv")
+rest_key <- tribble(
+  ~ restriction, ~ r_level, ~ Category,
+  NA , "None", "None",
+  1 , "Full", "Parks & Protected Areas",
+  2 , "High", "Other Protected Lands",
+  3 , "Medium", "Resource Exclusion Areas",
+  4 , "Low", "Managed Areas"
+)
 
-ecoregion_cat_summary <- ecosection_cat_summary %>%
-  group_by(ecoregion_code = parent_ecoregion_code, category) %>%
-  summarize(area_des = sum(area_des, na.rm = TRUE)) %>%
-  right_join(ecoreg_sizes, by = "ecoregion_code") %>%
-  mutate(percent_des = as.numeric(area_des / ecoreg_area * 100),
-            area_des_ha = area_des * 1e-4) %>%
-  left_join(select(bcmaps::ecoregions@data, ecoregion_code = CRGNCD, ecoregion_name = CRGNNM)) %>%
-  mutate(ecoregion_name = tools::toTitleCase(tolower(ecoregion_name))) %>%
-  select(ecoregion_code, ecoregion_name, category, everything()) %>%
-  write_feather("out-shiny/ld_ecoreg_summary.feather") %>%
-  write_csv("out/bc_ecoregions_land_designations_summary.csv")
+cons_area_all <- rbind(ld_f_sum, ld_m_sum, ld_o_sum) %>%
+  left_join(rest_key)
+
+#add nat res district to spatial data for plotting
+
+message("Add nr districts regions")
+ld_m_nr <- st_intersection(ld_m, bc_nr_dist)
+# write_rds(pa_eco, "data/CPCAD_Dec2020_BC_clean_no_ovlps_ecoregions.rds")
+
+
+
+
+
+
+
+
+save(ld_f_sum, ld_m_sum, ld_o_sum, cons_area_all, file = "tmp/clean.RData")
+
+
